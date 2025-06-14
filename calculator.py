32100a02868f85d0e3e5127bc3f8aed70356ca74
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-06-14 15:10:52 -0700

    push

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#comput the value between x and y using float
+x=float(input('What the x?'))
+y=float(input('What the y?'))
+z=round(x/y,2)
+
+#Calculate the result and round
+print(z)
--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
 print('Hello, World!')
+#define the function to call
+def hello():
+    print("hello")
+    
+name = input("What your Name?: ")
+hello()
+print(name)
+print()
+print()
+
+def welcom():
+    print("welcom")
+    
+name = input("What your name?: ")
+welcom()
+print(name)
